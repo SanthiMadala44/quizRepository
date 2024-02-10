127f9e2c0a086417a365b132f050df4dad17e29c
santhi <santhi.tara@gmail.com>
2024-02-11 01:36:09 +0530

    code cleaning

--- src/main/java/com/example/quizapp/controller/QuizController.java
+++ src/main/java/com/example/quizapp/controller/QuizController.java
@@ -8,7 +8,6 @@ import com.example.quizapp.service.QuizService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
