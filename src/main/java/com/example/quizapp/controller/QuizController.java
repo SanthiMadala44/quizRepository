c812dffff0b60d9dabdf45d599e6d29ab1d23151
santhi <santhi.tara@gmail.com>
2024-02-11 01:31:48 +0530

    getting quiz from Questions after many to many mapping

--- src/main/java/com/example/quizapp/controller/QuizController.java
+++ src/main/java/com/example/quizapp/controller/QuizController.java
@@ -2,13 +2,20 @@ package com.example.quizapp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.quizapp.model.Question;
 import com.example.quizapp.service.QuizService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -20,5 +27,11 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<Question>> getQuizQuestions(@PathVariable int id){
+        return quizService.getQuizQuestions(id);
     }
 }
--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -1,5 +1,6 @@
 package com.example.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Service;
 import com.example.quizapp.dao.QuestionDAO;
 import com.example.quizapp.dao.QuizDao;
 import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
 import com.example.quizapp.model.Quiz;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -29,5 +32,16 @@ public class QuizService {
         quizdao.save(quiz);
         
         return new ResponseEntity<>("SUCCESS", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Question>> getQuizQuestions(int id) {
+        Optional<Quiz> quiz = quizdao.findById(id);
+        List<Question> qustionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+        for(Question q: qustionsFromDB){
+            QuestionWrapper  qw = new QuestionWrapper(q.getId(), q.getQuestion_title(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionForUser.add(qw);
+        }
+        return new ResponseEntity<>(qustionsFromDB, HttpStatus.OK);
     }
 }
